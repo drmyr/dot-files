fb24b95a3b0e7f8425ee9de5baf1b5a63529fd67
drm <drm@localhost.localdomain>
2017-09-07 18:50:18 -0400

    new lecture

--- Bookmarks.scala
+++ Bookmarks.scala
@@ -1,5 +1,6 @@
 object Bookmarks {
-	val scalaReactiveProgramming = Map(4 -> "https://www.youtube.com/watch?v=jOvpuVwj8po&index=3&list=PLMhMDErmC1TdBMxd3KnRfYiBV2ELvLyxN",
-					   5 -> "https://www.youtube.com/watch?v=FOYASQx9hrg&list=PLMhMDErmC1TdBMxd3KnRfYiBV2ELvLyxN&index=4"
+	val reactive = Map(4 -> "https://www.youtube.com/watch?v=jOvpuVwj8po&index=3&list=PLMhMDErmC1TdBMxd3KnRfYiBV2ELvLyxN",
+					   5 -> "https://www.youtube.com/watch?v=FOYASQx9hrg&list=PLMhMDErmC1TdBMxd3KnRfYiBV2ELvLyxN&index=4",
+					   6 -> "https://www.youtube.com/watch?v=oUr1IQPHa9M&index=5&list=PLMhMDErmC1TdBMxd3KnRfYiBV2ELvLyxN"
 					  )
 }
