8b832758e084a4a5e636b36062aa1988a59b9747
David Meyer <david.meyer@Davids-MacBook-Pro.local>
2017-09-15 16:25:36 -0400

    git stuff

--- Bookmarks.scala
+++ Bookmarks.scala
@@ -3,5 +3,9 @@ object Bookmarks {
 					   5 -> "https://www.youtube.com/watch?v=FOYASQx9hrg&list=PLMhMDErmC1TdBMxd3KnRfYiBV2ELvLyxN&index=4",
 					   6 -> "https://www.youtube.com/watch?v=oUr1IQPHa9M&index=5&list=PLMhMDErmC1TdBMxd3KnRfYiBV2ELvLyxN"
 					  )
-	val certs = Map(1 -> "http://nategood.com/client-side-certificate-authentication-in-ngi")
+
+  val certs = Map(1 -> "http://nategood.com/client-side-certificate-authentication-in-ngi")
+
+  val gitflow = Map(1 -> "https://about.gitlab.com/2014/09/29/gitlab-flow/")
+
 }
